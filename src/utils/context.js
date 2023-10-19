1d6f6339fd295c6ff56353f45605fe9b294f21da
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-10-20 00:17:21 +0530

    Header and Banner

---
+++ src/utils/context.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createContext, useState } from 'react';
+
+export const Context = createContext();
+
+const AppContext = ({children}) => {
+  return (
+    <Context.Provider>
+        {children}
+    </Context.Provider>
+  )
+};
+
+export default AppContext
